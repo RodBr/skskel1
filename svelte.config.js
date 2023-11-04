e79b1a1723751ec45c6438fb56d23e97e2807f41
RodBr <rod.brown@live.com>
2023-11-04 12:40:31 +1100

    change config

--- svelte.config.js
+++ svelte.config.js
@@ -18,15 +18,18 @@
 // export default config;
 
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [vitePreprocess()],
+
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === 'production' ? '/svsk1' : ''
 		}
 	}
 };
